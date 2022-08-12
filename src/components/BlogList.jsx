972f11873c66c79fbafa0322b4076e568f365f7d
da0x3 <da0x3@Damians-MacBook-Pro.local>
2022-08-12 14:52:13 -0700

    refactored pagination logic to handle lower page sizes, added prop typing in pagination hook, and documented changes.

--- src/components/BlogList.jsx
+++ src/components/BlogList.jsx
@@ -6,26 +6,40 @@ import blogs from "../data/blogs.json";
 const PAGE_SIZES = [15, 25, 50, 100];
 
 function BlogList() {
+
+  /**
+   * Several states and logic required to update page, content for each page, as well as handling page size
+   * Todo - A refactor pass to combine into one state or create a separate hook to handle these states.
+   */
   const [currentPage, setCurrentPage] = useState(1)
-  const [currentPageSize, setPageSize] = useState(Number(15))
+  const [currentPageSize, setPageSize] = useState(15)
   const [currentPaginationData, setPaginationData] = useState(blogs.posts.slice(0, currentPageSize))
 
+  /**
+   * Updates page by changing current page state as well as updates data state to be displayed.
+   * @param {Number} page Current page number.
+   */
   const updatePage = (page) => {
     setCurrentPage(page)
+    /** handles default data state */
     if (page == 1) {
       setPaginationData(blogs.posts.slice(0, currentPageSize))
     } else {
-      setPaginationData(blogs.posts.slice(currentPageSize * page, currentPageSize * (page + 1)))
+      /** calculates correct data to retrive based on page and current page size. */
+      setPaginationData(blogs.posts.slice(currentPageSize * (page - 1), currentPageSize * page))
     }
   };
 
+  /**
+   * Updates the page size and default data to display upon selection of desired page size.
+   * @param {String} size Desired page size.
+   */
   const updateRowsPerPage = (size) => {
     setPageSize(Number(size))
     setCurrentPage(1)
     setPaginationData(blogs.posts.slice(0, size))
   };
 
-  console.log(currentPaginationData)
   return (
     <div>
       <Pagination
--- src/hooks/usePagination.js
+++ src/hooks/usePagination.js
@@ -1,13 +1,13 @@
+import PropTypes, { InferProps } from "prop-types";
 import { useMemo } from 'react'
+
 export const DOTS = "...";
 
-/*
-    Rewrite the logic here to map out the pagination to be displayed
-
-    !!!!!! ATTENTION !!!!!!
-    Please replace this comment here with a description of this hook.
-    
-  */
+/**
+ * Calculates correct pagination button format
+ * 
+ * @returns {Array} Containing correct pagination button format.
+ */
 function usePagination({
   currentPage,
   totalCount,
@@ -15,38 +15,57 @@ function usePagination({
 }) {
   const paginationRange = useMemo(() => {
 
+    /** totalPageCount calculated using Math.ceil to ensure extra page is not cut off */
     const totalPageCount = Math.ceil(totalCount / pageSize)
 
-    // debug
-    console.log(totalPageCount, totalCount, pageSize, currentPage)
- 
-    // default
-    if (currentPage == 1) {
-      return [1, 2, 3, DOTS, totalPageCount]
+    /** handles lower page count */
+    if (totalPageCount <= 3) {
+
+      /** default */
+      return range(1, totalPageCount)
+
+    } else {
+
+      /** defautl - [1 2 3 ... 13] beginning layout */
+      if (currentPage < 3) {
+        return [1, 2, 3, DOTS, totalPageCount]
+      }
+
+      /** [1 ... 2 3 4 ... 13] middle layout */
+      if (currentPage > 2 && currentPage <= totalPageCount - 2) {
+        return [1, DOTS, currentPage - 1, currentPage, currentPage + 1, DOTS, totalPageCount]
+      }
+
+      /** [1 ... 11 12 13] end layout */
+      if (currentPage > totalPageCount - 2) {
+        return [1, DOTS, totalPageCount - 2, totalPageCount - 1, totalPageCount]
+      }
     }
 
-    // [1] single page
-    if (totalPageCount == 1) {
-      return [1]
-    }
-
-    // [1 2 3 ... 13] beginning layout
-    if (currentPage < 3) {
-      return [1, 2, 3, DOTS, totalPageCount]
-    }
-
-    // [1 ... 2 3 4 ... 13] middle layout
-    if (currentPage > 2 && currentPage <= totalPageCount - 2) {
-      return [1, DOTS, currentPage - 1, currentPage, currentPage + 1, DOTS, totalPageCount]
-    }
-
-    // [1 ... 11 12 13] end layout
-    if (currentPage > totalPageCount - 2) {
-      return [1, DOTS, totalPageCount - 2, totalPageCount - 1, totalPageCount]
-    }
-    
   }, [currentPage, totalCount, pageSize])
   return paginationRange;
 }
+
+/**
+ * Takes two numbers and returns an Array containing the params and the range of numbers between them.
+ * @param {Number} start Number to start from
+ * @param {Number} end Number to end at
+ * @return {Array} Containing desired range
+*/
+function range(start, end) {
+  return Array.from({ length: end - start + 1 }, (_, i) => i + start);
+};
+
+usePagination.propTypes = {
+  totalCount: PropTypes.number,
+  currentPage: PropTypes.number,
+  pageSize: PropTypes.number,
+};
+
+usePagination.defaultProps = {
+  totalCount: 0,
+  currentPage: 1,
+  pageSize: 1,
+};
 
 export default usePagination;
